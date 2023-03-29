50fc1f2f6bd26617f32d0fdcb076cbae1417f202
Muhammad Motiur Rahman Mizan <89523410+DressedHuman@users.noreply.github.com>
2023-03-29 17:35:25 +0100

    Update settings.py
    Changing production allowed hosts.

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1(-mb9y0j(67s7!&j+cne2qxzo9231_hy_7$@83b@u=65el^p%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com',]
 
 
 # Application definition
